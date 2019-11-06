b5244fc72b0d88f56c0c766deeede6f5b45cdb52
Siwin Lo <siwin.lo@gmail.com>
2019-11-06 17:48:49 -0500

    9(design patterns): added google maps workaround

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -85170,6 +85170,7 @@ var user = new User_1.User();
 var company = new Company_1.Company();
 console.log(user);
 console.log(company);
+google;
 },{"./User":"src/User.ts","./Company":"src/Company.ts"}],"../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -85198,7 +85199,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57076" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55754" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,11 @@
 import { User } from "./User";
 import { Company } from "./Company";
+import {} from "googlemaps";
 
 const user = new User();
 const company = new Company();
 
 console.log(user);
 console.log(company);
+
+google;
